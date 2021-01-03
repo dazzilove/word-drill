7f49a6b59a95ec35e00bf6b4f54cfbb775d84ad6
1100303 <dazzilove@sk.com>
2021-01-04 00:31:22 +0900

    axios 추가

---
+++ app/front-end/src/service/axios.service.ts
@@ -0,0 +1,9 @@
+import axios, { AxiosInstance } from 'axios';
+import { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
+
+export default class AxiosService {
+    static readonly instance: AxiosInstance = axios.create({
+        baseURL: 'http://localhost:8081',
+        timeout: 100000,
+    });
+}
