824b648c7ef7165879f0dffb5ad2b84db849f766
1100303 <dazzilove@sk.com>
2021-01-05 03:07:01 +0900

    TSLint 가이드 적용

--- app/front-end/src/service/axios.service.ts
+++ app/front-end/src/service/axios.service.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from 'axios';
 import { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
 
 export default class AxiosService {
-    static readonly instance: AxiosInstance = axios.create({
+    public static readonly instance: AxiosInstance = axios.create({
         baseURL: 'http://localhost:8081',
         timeout: 100000,
     });
