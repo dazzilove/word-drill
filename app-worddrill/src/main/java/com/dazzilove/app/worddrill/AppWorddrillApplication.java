d3db0f7fc63b3433ae6acdb7a6f6aeae0c98cc30
dazzilove <dazzilove@gmail.com>
2021-04-18 02:03:03 +0900

    first

---
+++ app-worddrill/src/main/java/com/dazzilove/app/worddrill/AppWorddrillApplication.java
@@ -0,0 +1,13 @@
+package com.dazzilove.app.worddrill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWorddrillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWorddrillApplication.class, args);
+	}
+
+}
